14820908c46feefc383cfe6e4e6208af364ea5cd
AndTaran <65190142+AndTaran@users.noreply.github.com>
2020-09-29 16:46:46 +0300

    finished the task

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
-
 exports.min = function min(array) {
-  let min = array[0];
-  for (let i = 0; i < array.length; i++) {
-    if (array.length !== 0) {
+  if (!array || array.length === 0) {
+    return 0
+  } else {
+    let min = array[0];
+    for (let i = 0; i < array.length; i++){
       if (min > array[i]) min = array[i];
-    } else return 0
+    }
+    return min;
   }
-  return min;
 }
 
 exports.max = function max (array) {
-  let max = array[0];
-  for (let i = 0; i < array.length; i++) {
-    if (max < array[i]) {
-      max = array[i];
+  if (!array || array.length === 0) {
+    return 0
+  } else {
+    let max = array[0];
+    for (let i = 0; i < array.length; i++){
+      if (max < array[i]) max = array[i]
     }
+    return max;
   }
-  return max;
 }
 
 exports.avg = function avg (array) {
-  let total = 0;
-  for(let i = 0; i < array.length; i++) {
-    total += array[i];
+  if (!array || array.length === 0) {
+    return 0
+  } else {
+      let total = 0;
+      for(let i = 0; i < array.length; i++) {
+      total += array[i];
+    }
+    let avg = total / array.length;
+    return avg;
   }
-  let avg = total / array.length;
-  return avg;
 }
